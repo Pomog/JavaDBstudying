5441a245dd2eceb15e09e2f7d5eca81e7274aca7
Pomog <thoryur@gmail.com>
2023-05-05 07:02:15 +0300

    DB connection

--- PeopleDB/src/main/java/yp/peopledb/repository/PeopleRepository.java
+++ PeopleDB/src/main/java/yp/peopledb/repository/PeopleRepository.java
@@ -2,10 +2,7 @@ package yp.peopledb.repository;
 
 import yp.peopledb.model.Person;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.time.ZoneId;
 
 public class PeopleRepository {
@@ -18,11 +15,16 @@ public class PeopleRepository {
         String sql = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB VALUES (?, ?, ?)";
 
         try {
-            PreparedStatement ps = connection.prepareStatement(sql);
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, person.getFirstName());
             ps.setString(2, person.getLastName());
             ps.setTimestamp(3, Timestamp.valueOf(person.getDob().withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime()));
             int recordsAffected = ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()){
+                long id = rs.getLong(1);
+                person.setId(id);
+            }
             System.out.printf("recordsAffected: %d%n", recordsAffected);
         } catch (SQLException e) {
             throw new RuntimeException(e);
